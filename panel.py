e11f75a37c375d34387e0ea07b13d6ad498035e5
임승현 <shl0216@dgist.ac.kr>
2021-03-19 14:46:09 +0900

    Update panel.py

--- panel.py
+++ panel.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'panel.ui'
-#
-# Created by: PyQt5 UI code generator 5.10.1
-#
-# WARNING! All changes made in this file will be lost!
-
 import rospy
 import threading
 from PyQt5 import QtCore, QtGui, QtWidgets
